c8aac90165ca846387d7268df034425c29cf1554
UPLAKSH TYAGI <uplaksh.tyagi@ramanujan.du.ac.in>
2022-08-17 00:50:49 +0530

    create #00058.py

---
+++ #00058.py
@@ -0,0 +1,17 @@
+# Given a string s consisting of words and spaces, return the length of the last word in the string.
+
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+
+        count = 0
+        for i in s[::-1]:
+            if i != ' ':
+                count += 1
+            elif i == ' ' and count == 0:
+                continue
+            else:
+                break
+        return count
+
+
+print(Solution.lengthOfLastWord(self=True, s="hello there "))
