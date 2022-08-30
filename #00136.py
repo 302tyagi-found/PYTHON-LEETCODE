2b25966832f3ade11ce4e7db346a8fd6f492ee92
UPLAKSH TYAGI <uplaksh.tyagi@ramanujan.du.ac.in>
2022-08-30 12:09:45 +0530

    PROBLEM SOLVED

---
+++ #00136.py
@@ -0,0 +1,11 @@
+# Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+
+# You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+
+class Solution:
+    def singleNumber(self, nums: list[int]) -> int:
+        for i in nums:
+            appears = nums.count(i)
+            if appears == 1:
+                return i
