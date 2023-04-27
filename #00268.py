d6b25d63ef4482d3a2a3c60f8ad8b5135c92e570
Uplaksh Tyagi <uplaksh.tyagi@ramanujan.du.ac.in>
2023-04-27 17:52:17 +0530

    PROBLEM SOLVED

--- #00268.py
+++ #00268.py
@@ -1,10 +1,13 @@
 # Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
 class Solution:
     def missingNumber(self, nums: list[int]) -> int:
+        missing = len(nums)
+        for i in range(len(nums)):
+            missing ^= nums[i] ^ i
+        return missing
+
+        # or
+        
         n = len(nums)
-        nums_dict = {}
-        for i in range(n):
-            nums_dict[nums[i]] = 1
-            for i in range(0, n+1):
-                if i not in nums:
-                    return i
+        m = sum(nums)
+        return (n*(n+1))//2 - m
