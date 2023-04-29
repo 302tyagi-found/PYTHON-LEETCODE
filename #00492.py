fb1620b11b397915f86afcff58874a3f863a84a1
Uplaksh Tyagi <uplaksh.tyagi@ramanujan.du.ac.in>
2023-04-29 16:51:48 +0530

    PROBLEM SOLVED

---
+++ #00492.py
@@ -0,0 +1,16 @@
+# A web developer needs to know how to design a web page's size. So, given a specific rectangular web page’s area, your
+# job by now is to design a rectangular web page, whose length L and width W satisfy the following requirements:
+# The area of the rectangular web page you designed must equal to the given target area.
+# The width W should not be larger than the length L, which means L >= W.
+# The difference between length L and width W should be as small as possible.
+# Return an array [L, W] where L and W are the length and width of the web page you designed in sequence.
+class Solution:
+    def constructRectangle(self, area: int) -> list[int]:
+        width = int(area ** 0.5)
+        length = int(area / width)
+        while length * width != area:
+            if length * width < area:
+                length += 1
+            else:
+                width -= 1
+        return [length, width]
