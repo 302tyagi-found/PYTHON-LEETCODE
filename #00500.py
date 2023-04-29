928ceb2bd52b9ec606ca13e8e36af0f15e934db2
Uplaksh Tyagi <uplaksh.tyagi@ramanujan.du.ac.in>
2023-04-30 03:27:07 +0530

    PROBLEM SOLVED

---
+++ #00500.py
@@ -0,0 +1,27 @@
+# Given an array of strings words, return the words that can be typed using letters of the alphabet on only one row of American
+# keyboard like the image below.
+# In the American keyboard:
+# the first row consists of the characters "qwertyuiop",
+# the second row consists of the characters "asdfghjkl", and
+# the third row consists of the characters "zxcvbnm".
+class Solution:
+    def findWords(self, words: list[str]) -> list[str]:
+        rows = [set('qwertyuiop'), set('asdfghjkl'), set('zxcvbnm')]
+        output = []
+        for word in words:
+            chars = set(word.lower())
+            if len(word) == 1:
+                output.append(word)
+            elif chars.issubset(rows[0]):
+                output.append(word)
+            elif chars.issubset(rows[1]):
+                output.append(word)
+            elif chars.issubset(rows[2]):
+                output.append(word)
+
+        return output
+
+
+sol = Solution()
+words = ["asdfghjkl", "qwertyuiop", "zxcvbnm"]
+print(sol.findWords(words))
