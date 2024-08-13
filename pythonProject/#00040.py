9376abcca7482a3c3e5b63e3ebcbeac7bc5d9988
Uplaksh Tyagi <uplaksh.tyagi@ramanujan.du.ac.in>
2024-08-13 23:40:03 +0530

    PROBLEM SOLVED!

---
+++ pythonProject/#00040.py
@@ -0,0 +1,27 @@
+# Given a collection of candidate numbers (candidates) and a target number (target), find all unique combinations in candidates where the candidate numbers sum to target.
+#
+# Each number in candidates may only be used once in the combination.
+#
+# Note: The solution set must not contain duplicate combinations.
+#
+#
+from typing import List
+
+
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        def backtrack(start, target, path):
+            if target == 0:
+                result.append(path)
+                return
+            for i in range(start, len(candidates)):
+                if i > start and candidates[i] == candidates[i - 1]:
+                    continue
+                if candidates[i] > target:
+                    break
+                backtrack(i + 1, target - candidates[i], path + [candidates[i]])
+
+        candidates.sort()
+        result = []
+        backtrack(0, target, [])
+        return result
